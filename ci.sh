bba9987a6bd54b2e6c65e677ba293f3dfc36cb30
Baptiste Fontaine <baptiste@bixoto.com>
2023-09-12 15:36:22 +0000

    Add ci.sh

---
+++ ci.sh
@@ -0,0 +1,15 @@
+#! /bin/bash -ex
+[ ! -f pyproject.toml ] && exit 0
+
+COV_ARGS=""
+
+if [ -n "$HTMLCOV" ]; then
+  COV_ARG="$COV_ARGS --cov-report=html"
+fi
+if [ -n "$BRANCHCOV" ]; then
+  COV_ARG="$COV_ARGS --cov-branch"
+fi
+
+
+poetry run mypy --check-untyped-defs --explicit-package-bases ./*.py magento/*.py
+poetry run pytest --cov=. $COV_ARGS tests/
