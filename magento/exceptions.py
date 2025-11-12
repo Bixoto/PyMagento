a7a39d7fd93632dc63e31f703e628d9eb2533046
Baptiste Fontaine <baptiste@bixoto.com>
2025-11-12 15:54:04 +0100

    simplify type cast

--- magento/exceptions.py
+++ magento/exceptions.py
@@ -1,6 +1,7 @@
 import re
+from typing import List, Optional, Union, Dict
+
 import requests
-from typing import List, Optional, Union, Dict, cast
 
 RE_PARAM = re.compile(r"%([A-Za-z0-9]+)")
 
@@ -17,13 +18,13 @@ def build_exception_text(message: str, parameters: Optional[Union[Dict[str, str]
     #   message = "%fieldName is a required field."
     #   parameters = {"fieldName":"product"}
     if isinstance(parameters, dict):
-        return cast(str, RE_PARAM.sub(lambda m: str(parameters[m.group(1)]), message))  # type: ignore
+        return RE_PARAM.sub(lambda m: str(parameters[m.group(1)]), message)
 
-    # For example:
+        # For example:
     #   message = 'El atributo "%1" no incluye una opción con el ID "%2".'
     #   parameters = ['manufacturer', '17726']
     if isinstance(parameters, list):
-        return cast(str, RE_PARAM.sub(lambda m: str(parameters[int(m.group(1)) - 1]), message))  # type: ignore
+        return RE_PARAM.sub(lambda m: str(parameters[int(m.group(1)) - 1]), message)
 
     # Ok fallback
     return str((message, parameters))
